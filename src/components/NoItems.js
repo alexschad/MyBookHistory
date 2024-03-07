2f83e1c3f25de8904f61822273c5f98025bcd8bd
Alexander Schad <ags@vanguardistas.net>
2024-03-07 14:56:26 +0100

    fix no items image

--- src/components/NoItems.js
+++ src/components/NoItems.js
@@ -1,21 +1,28 @@
 /* global require */
 
 import React from 'react';
-import { View } from 'react-native';
+import { View, Pressable, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { useTheme } from '../ThemeManager';
-import BackgroundImage from './BackgroundImage';
 
 export default function NoItems({ items }) {
   const {
     theme: { styles },
   } = useTheme();
+  const navigation = useNavigation();
 
   if (items === null) {
     return <></>;
   }
   return (
     <View style={styles.emptyListContainer}>
-      <BackgroundImage source={require('../../assets/images/NoItems.png')} />
+      <Pressable
+        onPress={() => {
+          navigation.navigate('Scanner');
+        }}
+        style={styles.navHeaderLink}>
+        <Image source={require('../../assets/images/NoItems.png')} />
+      </Pressable>
     </View>
   );
 }
